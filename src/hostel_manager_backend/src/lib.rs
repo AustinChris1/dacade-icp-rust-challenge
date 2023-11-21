80d55f3ece756a12d732ef869445da4c9263f4d2
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-21 06:52:47 +0100

    Update lib.rs
    **Errors**
    
    * The code does not handle errors gracefully. For example, if the memory manager fails to initialize, the code will panic.
    * The code does not check for invalid input. For example, if the user tries to create a room with a negative capacity, the code will crash.
    
    **Vulnerabilities**
    
    * The code uses a thread-local variable to store the memory manager. This is a potential security vulnerability because it allows any thread to access the memory manager, which could lead to unauthorized access to sensitive data. To fix this vulnerability, the memory manager should be stored in a global variable or a shared memory object.
    * The code does not check for concurrent access to the ROOMS vector. This could lead to race conditions and data corruption. To fix this issue, the ROOMS vector should be synchronized using a mutex or a lock.
    
    **Bugs**
    
    * The code does not check for rooms that are already booked before adding a new occupant. This could lead to a room being overbooked.
    * The code does not check for occupants that are already in the room before removing them. This could lead to an occupant being removed from the room twice.
    
    **Suggested solutions**
    
    * To handle errors gracefully, the code should use try-catch blocks or result types.
    * To check for invalid input, the code should use validation functions.
    * To fix the thread-local memory manager vulnerability, the memory manager should be stored in a global variable or a shared memory object.
    * To prevent concurrent access to the ROOMS vector, the vector should be synchronized using a mutex or a lock.
    * To prevent overbooking, the code should check for rooms that are already booked before adding a new occupant.
    * To prevent occupants from being removed twice, the code should check for occupants that are already in the room before removing them.
    
    In addition to the above, I would recommend using a more robust data structure for storing rooms, such as a hash table or a tree. This would make it easier to find rooms by their number and to remove rooms from the vector.

--- src/hostel_manager_backend/src/lib.rs
+++ src/hostel_manager_backend/src/lib.rs
@@ -1,6 +1,6 @@
 use candid::{Decode, Encode};
 use ic_stable_structures::memory_manager;
-use std::{borrow, cell};
+use std::{cell, sync::{Arc, Mutex}};
 
 type Memory =
     ic_stable_structures::memory_manager::VirtualMemory<ic_stable_structures::DefaultMemoryImpl>;
@@ -14,14 +14,12 @@ enum Error {
     RoomAlreadyBooked,
     InsufficientPrice,
     NotOwner,
-    NotInRoom
+    NotInRoom,
+    InvalidCapacity,
 }
 
 #[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
 struct User(String);
-
-#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone)]
-struct MyVec<T>(Vec<T>);
 
 #[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
 enum RoomState {
@@ -36,7 +34,7 @@ struct Room {
     capacity: usize,
     price_per_occupant: usize,
     state: RoomState,
-    occupants: MyVec<User>,
+    occupants: Vec<User>,
     owner: User,
 }
 
@@ -70,116 +68,110 @@ impl ic_stable_structures::BoundedStorable for Room {
     const IS_FIXED_SIZE: bool = false;
 }
 
-thread_local! {
-    static MEMORY_MANAGER: cell::RefCell<memory_manager::MemoryManager<ic_stable_structures::DefaultMemoryImpl>> = cell::RefCell::new(memory_manager::MemoryManager::init(ic_stable_structures::DefaultMemoryImpl::default()));
-    static ROOM_COUNTER: cell::RefCell<IdCell> = cell::RefCell::new(IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(memory_manager::MemoryId::new(0))), 1).expect("Failed to initialize ROOM counter"));
-    // static ROOMS: cell::RefCell<StableVec<Room>> = cell::RefCell::new(StableVec::init(MEMORY_MANAGER.with(|m| m.borrow().get(memory_manager::MemoryId::new(1)))).expect("Failed to initialize vector!"));
-    static ROOMS: cell::RefCell<MyVec<Room>> = cell::RefCell::new(MyVec(vec![]));
+lazy_static::lazy_static! {
+    static ref MEMORY_MANAGER: Mutex<memory_manager::MemoryManager<ic_stable_structures::DefaultMemoryImpl>> =
+        Mutex::new(memory_manager::MemoryManager::init(ic_stable_structures::DefaultMemoryImpl::default()).expect("Failed to initialize Memory Manager"));
+    static ref ROOM_COUNTER: IdCell = IdCell::init(
+        MEMORY_MANAGER.lock().unwrap().get(memory_manager::MemoryId::new(0)),
+        1,
+    )
+    .expect("Failed to initialize ROOM counter");
+    static ref ROOMS: Mutex<Vec<Room>> = Mutex::new(vec![]);
 }
 
 #[ic_cdk::query]
 fn get_rooms() -> Vec<Room> {
-    ROOMS.with(|r| r.borrow().0.clone())
+    ROOMS.lock().unwrap().clone()
 }
 
 #[ic_cdk::query]
 fn get_room_by_number(room_no: u64) -> Result<Room, Error> {
-    ROOMS.with(|r| {
-        let rooms = r.borrow();
-        match rooms.0.iter().find(|r| r.no == room_no) {
-            Some(room) => Ok(room.clone()),
-            None => Err(Error::RoomNotFound),
-        }
-    })
+    let rooms = ROOMS.lock().unwrap();
+    match rooms.iter().find(|r| r.no == room_no) {
+        Some(room) => Ok(room.clone()),
+        None => Err(Error::RoomNotFound),
+    }
 }
 
 #[ic_cdk::update]
-fn create_room(capacity: usize, price_per_occupant: usize) -> () {
-    ROOM_COUNTER.with(|c| {
-        let room_number = *c.borrow().get();
-        let room = Room {
-            no: room_number,
-            capacity,
-            price_per_occupant,
-            state: RoomState::TotallyVacant,
-            occupants: MyVec(vec![]),
-            owner: User(ic_cdk::caller().to_string()),
-        };
+fn create_room(capacity: usize, price_per_occupant: usize) -> Result<(), Error> {
+    if capacity <= 0 {
+        return Err(Error::InvalidCapacity);
+    }
 
-        ROOMS.with(|r| r.borrow_mut().0.push(room));
-    })
+    let room_number = ROOM_COUNTER.get();
+    let room = Room {
+        no: room_number,
+        capacity,
+        price_per_occupant,
+        state: RoomState::TotallyVacant,
+        occupants: Vec::new(),
+        owner: User(ic_cdk::caller().to_string()),
+    };
+
+    ROOMS.lock().unwrap().push(room);
+    Ok(())
 }
 
 #[ic_cdk::update]
 fn book_room(room_no: u64, price: usize) -> Result<(), Error> {
-    ROOMS.with(|r| {
-        let mut rooms = r.borrow_mut();
-        let room = match rooms.0.iter_mut().find(|r| r.no == room_no) {
-            Some(room) => room,
-            None => return Err(Error::RoomNotAvailable),
-        };
+    let mut rooms = ROOMS.lock().unwrap();
+    let room = match rooms.iter_mut().find(|r| r.no == room_no) {
+        Some(room) => room,
+        None => return Err(Error::RoomNotAvailable),
+    };
 
-        if room.state == RoomState::Full {
-            return Err(Error::RoomFull);
-        }
+    if room.state == RoomState::Full {
+        return Err(Error::RoomFull);
+    }
 
-        if price < room.price_per_occupant {
-            return Err(Error::InsufficientPrice);
-        }
+    if price < room.price_per_occupant {
+        return Err(Error::InsufficientPrice);
+    }
 
-        let occupant = User(ic_cdk::caller().to_string());
+    let occupant = User(ic_cdk::caller().to_string());
 
-        match room.occupants.0.iter().find(|o| **o == occupant) {
-            Some(_) => {
-                room.occupants.0.push(occupant);
-                Ok(())
-            }
-            None => return Err(Error::RoomAlreadyBooked),
-        }
-    })
+    if room.occupants.iter().any(|o| *o == occupant) {
+        return Err(Error::RoomAlreadyBooked);
+    }
+
+    room.occupants.push(occupant);
+    Ok(())
 }
 
 #[ic_cdk::update]
 fn unbook_room(room_no: u64) -> Result<(), Error> {
-    ROOMS.with(|r| {
-        let mut rooms = r.borrow_mut();
-        let room = match rooms.0.iter_mut().find(|r| r.no == room_no) {
-            Some(room) => room,
-            None => return Err(Error::RoomNotAvailable),
-        };
+    let mut rooms = ROOMS.lock().unwrap();
+    let room = match rooms.iter_mut().find(|r| r.no == room_no) {
+        Some(room) => room,
+        None => return Err(Error::RoomNotAvailable),
+    };
 
-        let occupant = User(ic_cdk::caller().to_string());
+    let occupant = User(ic_cdk::caller().to_string());
 
-        match room.occupants.0.iter().position(|o| *o == occupant) {
-            Some(index) => {
-                room.occupants.0.remove(index);
-                Ok(())
-            }
-            None => Err(Error::NotInRoom),
-        }
-    })
+    if let Some(index) = room.occupants.iter().position(|o| *o == occupant) {
+        room.occupants.remove(index);
+        Ok(())
+    } else {
+        Err(Error::NotInRoom)
+    }
 }
 
 #[ic_cdk::update]
 fn delete_room(room_no: u64) -> Result<(), Error> {
-    ROOMS.with(|r| {
-        let mut rooms = r.borrow_mut();
-        let room_index = rooms
-            .0
-            .iter()
-            .position(|room| room.no == room_no)
-            .ok_or(Error::RoomNotAvailable)?;
-
-        let room = &rooms.0[room_index];
+    let mut rooms = ROOMS.lock().unwrap();
+    if let Some(room_index) = rooms.iter().position(|room| room.no == room_no) {
+        let room = &rooms[room_index];
 
         if room.owner != User(ic_cdk::caller().to_string()) {
             return Err(Error::NotOwner);
         }
 
-        rooms.0.remove(room_index);
-
+        rooms.remove(room_index);
         Ok(())
-    })
+    } else {
+        Err(Error::RoomNotAvailable)
+    }
 }
 
 ic_cdk::export_candid!();
